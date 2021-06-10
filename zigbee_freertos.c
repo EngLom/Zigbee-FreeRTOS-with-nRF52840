4bc29396e95fe91066fcd64efffc24cc9a988e2a
EngLom <85689420+EngLom@users.noreply.github.com>
2021-06-10 19:17:12 +0200

    Update zigbee_freertos.c

--- zigbee_freertos.c
+++ zigbee_freertos.c
@@ -25,13 +25,13 @@
 #include "zb_multi_sensor.h"
 #include "SHT3x.h"
 
-#define IEEE_CHANNEL_MASK                  (1l << ZIGBEE_CHANNEL)               /**< Scan only one, predefined channel to find the coordinator. */
-#define ERASE_PERSISTENT_CONFIG            ZB_FALSE                             /**< Do not erase NVRAM to save the network parameters after device reboot or power-off. */
+#define IEEE_CHANNEL_MASK                  (1l << ZIGBEE_CHANNEL)                /* Scan only one, predefined channel to find the coordinator. */
+#define ERASE_PERSISTENT_CONFIG            ZB_FALSE                              /* Do not erase NVRAM to save the network parameters after device reboot or power-off. */
 
-#define ZIGBEE_NETWORK_STATE_LED           BSP_BOARD_LED_2                      /**< LED indicating that light switch successfully joind ZigBee network. */
-#define BLINKY_LED                         BSP_BOARD_LED_0                      /**< Blinking led */
-
-#define ZIGBEE_MAIN_TASK_PRIORITY               (tskIDLE_PRIORITY + 2U)
+#define ZIGBEE_NETWORK_STATE_LED           BSP_BOARD_LED_2                       /* LED indicating that light switch successfully joind ZigBee network. */
+#define BLINKY_LED                         BSP_BOARD_LED_0                       /* Blinking led */
+ 
+#define ZIGBEE_MAIN_TASK_PRIORITY          (tskIDLE_PRIORITY + 2U)               /* Set tast prority */
 static TaskHandle_t m_zigbee_main_task_handle;
 static SemaphoreHandle_t m_zigbee_main_task_mutex;
 
@@ -39,13 +39,13 @@ static SemaphoreHandle_t m_zigbee_main_task_mutex;
 #define PRESSURE_MEASUREMENT_TASK_PRIORITY      (tskIDLE_PRIORITY + 2U)
 static TaskHandle_t m_pressure_measurement_task_handle;
 
-#define LED_TOGGLE_TASK_STACK_SIZE        (configMINIMAL_STACK_SIZE + 64U)
+#define LED_TOGGLE_TASK_STACK_SIZE        (configMINIMAL_STACK_SIZE + 64U)        
 #define LED_TOGGLE_TASK_PRIORITY          (tskIDLE_PRIORITY + 2U)
 static TaskHandle_t m_led_toggle_task_handle;
 
 #if (NRF_LOG_ENABLED && NRF_LOG_DEFERRED)
 #define LOG_TASK_STACK_SIZE               (1024U / sizeof(StackType_t))
-#define LOG_TASK_PRIORITY                 (tskIDLE_PRIORITY + 1U)               /**< Must be lower than any task generating logs */
+#define LOG_TASK_PRIORITY                 (tskIDLE_PRIORITY + 1U)               /* Must be lower than any task generating logs */
 static TaskHandle_t m_logger_task_handle;
 #endif
 
@@ -96,8 +96,8 @@ APP_TIMER_DEF(temperature_measurement_timer);
 static update_temperature_measurement_ctx_t m_update_temperature_measurement_ctx;
 
 
-/**@brief  Task function responsible for led blinking. param  pvParameter     
-FreeRTOS task parameter, unused here, required by FreeRTOS API.*/
+/* brief  Task function responsible for led blinking. param  pvParameter */    
+/* FreeRTOS task parameter, unused here, required by FreeRTOS API.*/
 bool led_toggle_task(void *pvParameter)
 {
     UNUSED_PARAMETER(pvParameter);
@@ -117,21 +117,25 @@ bool freertos_int(void)
 {
     /*Timer initialization. This creates and starts application timers. */
     uint32_t err_code = app_timer_init();
-        if (err_code != NRF_SUCCESS) return err_code; 
+        if (err_code != NRF_SUCCESS) 
+          return err_code; 
         
     /*initializing the clock.*/
     uint32_t err_code = nrf_drv_clock_init();
-        if (err_code != NRF_SUCCESS) return err_code;
+        if (err_code != NRF_SUCCESS) 
+          return err_code;
 
     /*initializing LEDs*/
-        uint32_t error_code = bsp_init(BSP_INIT_LEDS, NULL);
-     if (err_code != NRF_SUCCESS) return err_code;
+    uint32_t error_code = bsp_init(BSP_INIT_LEDS, NULL);
+       if (err_code != NRF_SUCCESS) 
+         return err_code;
+  
     bsp_board_leds_off();
 
     return NRF_SUCCESS;
 }
 
-/**@brief ZigBee stack event handler.*/
+/* brief ZigBee stack event handler.*/
 bool zboss_signal_handler(zb_uint8_t param)
 {
     /* Just to show that we are in zigbee_main_task context */
@@ -165,8 +169,7 @@ bool zboss_signal_handler(zb_uint8_t param)
 
         case ZB_COMMON_SIGNAL_CAN_SLEEP:
             /* When freertos is used zb_sleep_now must not be used, due to
-             * zigbee communication being not the only task to be performed by node.
-             */
+             * zigbee communication being not the only task to be performed by node */
             break;
         
         case ZB_ZDO_SIGNAL_PRODUCTION_CONFIG_READY:
@@ -193,7 +196,8 @@ bool zigbee_main_task(void *pvParameter)
 {
     zb_ret_t       zb_err_code;
     UNUSED_PARAMETER(pvParameter);
-    NRF_LOG_INFO("The zigbee_main_task started.");
+    NRF_LOG_INFO("The zigbee_main_task started."); 
+  
     /* Start Zigbee Stack. */
     zb_err_code = zboss_start();
         if (err_code != NRF_SUCCESS) return err_code;
@@ -210,7 +214,7 @@ bool zigbee_main_task(void *pvParameter)
      return NRF_SUCCESS;
 }
 
-/**@brief Function for initializing all clusters attributes.*/
+/* brief Function for initializing all clusters attributes.*/
 void multi_sensor_clusters_attr_init(void)
 {
     /* Basic cluster attributes data */
@@ -257,7 +261,7 @@ void multi_sensor_clusters_attr_init(void)
     m_dev_ctx.pres_attr.tolerance                = ZB_ZCL_ATTR_PRES_MEASUREMENT_TOLERANCE_MAX_VALUE;
 }
 
-/**@brief   Callback scheduled from @ref temperature_measurement_timer_handler*/
+/* brief Callback scheduled from @ref temperature_measurement_timer_handler*/
 void update_temperature_measurement_cb(zb_uint8_t param)
 {
     UNUSED_PARAMETER(param);
@@ -287,7 +291,7 @@ void update_temperature_measurement_cb(zb_uint8_t param)
     }
 }
 
-/**@brief Function for handling nrf app timer*/
+/* brief Function for handling nrf app timer*/
 void temperature_measurement_timer_handler(void * context)
 {
     UNUSED_PARAMETER(context);
@@ -315,7 +319,7 @@ void temperature_measurement_timer_handler(void * context)
     }
 }
 
-/**@brief  Task performing pressure measurement.*/
+/* brief  Task performing pressure measurement.*/
 static void pressure_measurement_task(void *pvParam)
 {
     /* Just to show that we are NOT in zigbee_main_task context */
@@ -362,23 +366,23 @@ static void pressure_measurement_task(void *pvParam)
     }
 }
 
-/**@brief FreeRTOS hook function called from idle task */
+/* brief FreeRTOS hook function called from idle task */
 void vApplicationIdleHook(void)
 {
     /* No task is running, just idle on lowest priority, so we can lower power usage */
     __WFE();
 }
 
-/**@brief  FreeRTOS hook function called when stack overflow has been detected
- * @note   See FreeRTOS API*/
+/* brief  FreeRTOS hook function called when stack overflow has been detected
+/* note   See FreeRTOS API*/
 void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName)
 {
     NRF_LOG_ERROR("vApplicationStackOverflowHook(%x,\"%s\")", xTask, pcTaskName);
     APP_ERROR_HANDLER(NRF_ERROR_NO_MEM);
 }
 
-/**@brief Function which tries to sleep down the MCU
-/**@note  This function overrides implementation found in zb_nrf52840_common.c*/
+/* brief Function which tries to sleep down the MCU
+/* note  This function overrides implementation found in zb_nrf52840_common.c*/
 zb_void_t zb_osif_go_idle(zb_void_t)
 {
     /* Intentionally empty implementation */
